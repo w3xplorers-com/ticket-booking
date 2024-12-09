19a40f0957cab42ddc281891ef765f42366c75cc
w3xplorers <royaltalukder@w3xplorers.com>
2024-12-09 15:13:51 +0600

    set api version

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::get('/tickets', [TicketController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();

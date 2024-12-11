7de14d7c511ebf7df3f531fef4ea57127db25e15
w3xplorers <royaltalukder@w3xplorers.com>
2024-12-11 11:45:33 +0600

    tickets filtering added

---
+++ app/Http/Filters/V1/QueryFilter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Filters\V1;
+
+use Illuminate\Http\Request;
+
+abstract class QueryFilter
+{
+    protected $builder;
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function filter($arr)
+    {
+        foreach ($arr as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    public function apply($builder)
+    {
+        $this->builder = $builder;
+
+        foreach ($this->request->all() as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $builder;
+    }
+}
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Http\Filters\V1\QueryFilter;
 
 class Ticket extends Model
 {
     /** @use HasFactory<\Database\Factories\TikcketFactory> */
     use HasFactory;
 
-    public function user()
+    public function author()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters)
+    {
+        return $filters->apply($builder);
     }
 }
